85491b1bfaff47d3eab53350fbbf710c836dd548
ohveloper <ohveloper@gmail.com>
2024-06-02 13:58:35 +0900

    loading components

--- app/(sitre)/loading.tsx
+++ app/(sitre)/loading.tsx
@@ -2,10 +2,13 @@
 
 import React from 'react'
 import {BarLoader} from 'react-spinners'
+import LoadingBar from '@/components/LoadingBar'
 
 const loading = () => {
   return (
-    <div><BarLoader color='red'/></div>
+    <div>
+        <LoadingBar />
+    </div>
   )
 }
 
---
+++ components/LoadingBar.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+import {BarLoader} from 'react-spinners'
+
+const LoadingBar = () => {
+  return (
+    <div><BarLoader color='red'/></div>
+  )
+}
+
+export default LoadingBar
